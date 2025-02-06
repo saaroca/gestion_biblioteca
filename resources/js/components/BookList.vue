512d65b2e4daf21f0297938e40371edebc6721a2
saaroca <saaroca@gmail.com>
2025-02-06 12:44:17 +0100

    feat button add favourites & cookies

--- resources/js/components/BookList.vue
+++ resources/js/components/BookList.vue
@@ -4,12 +4,19 @@
         <ul>
             <li v-for="book in books" :key="book.id">
                 {{ book.title }}
+                <button
+                    class="bg-blue-500 text-white rounded-lg font-bold w-32 h-8"
+                    @click="addFavorito(book.id)"
+                >
+                    Add Fav
+                </button>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
+import Cookies from "js-cookie";
 export default {
     data() {
         return {
@@ -22,6 +29,26 @@ export default {
             .then((data) => {
                 this.books = data;
             });
+    },
+    methods: {
+        addFavorito(id) {
+            let favorites = Cookies.get("favorites");
+            if (!favorites) {
+                favorites = "[]";
+            }
+
+            favorites = JSON.parse(favorites);
+
+            if (!favorites.includes(id)) {
+                favorites.push(id);
+                Cookies.set("favorites", JSON.stringify(favorites), {
+                    expires: 30,
+                });
+            }
+
+            this.favorites = favorites;
+            console.log(this.favorites);
+        },
     },
 };
 </script>
--- resources/js/components/HelloWorld.vue
+++ resources/js/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
     <ul>
         <li>
+            <router-link to="/books">Listar libros</router-link>
+        </li>
+        <li>
             <router-link to="/login">Login</router-link>
         </li>
         <li>
